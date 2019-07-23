1dbc08ade3817c1235d3f241e10b5d05e7eba481
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 16:20:55 -0700

    Add files via upload

---
+++ src/main/main.kt
@@ -0,0 +1,26 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.kt                                                                   *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: MIT                                                                    *
+ *                                                                                 *
+ ***********************************************************************************/
+
+
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, Kotlin' is desplayed to the standerd console.
+ *
+ */
+fun main()
+{
+    println("Hello, Kotlin.");
+    
+}// end of function main
